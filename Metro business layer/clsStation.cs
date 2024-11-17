f2d8b967618cf52e80a22cbbcd74ab4965a654a2
eslambob44 <eyasser237@gmail.com>
2024-11-17 23:21:29 +0200

    List stations in business layer

--- Metro business layer/clsStation.cs
+++ Metro business layer/clsStation.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using MetroDataAccessLayer;
+using System.Data;
 namespace Metro_business_layer
 {
-    public class clsStation
+    static public class clsStation
     {
+        public static DataTable ListStations(float LineNumber)
+        {
+            if(LineNumber==3) return clsStationDataAccessLayer.ListAllStationsInLineThatHaveBranch(LineNumber);
+            else return clsStationDataAccessLayer.ListAllStationsInLine(LineNumber);
+        }
     }
 }
